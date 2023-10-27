e5b50fba4000eae27a98ad4a3c427dda27513a64
Pushpendra <pushpendravyasr22@gmail.com>
2023-10-27 14:20:44 +0530

    Edit-delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
 
   user : { name: string, email: string, contact:string } = { name: '', email: '', contact: ''};
   users: { name: string, email: string, contact:string }[] = [];
+  isEditing:boolean = false;
 
   private resetData() {
     this.user.name = '';
@@ -20,6 +21,29 @@ export class AppComponent {
   addUser() {
     this.users.push({ ...this.user });
     this.resetData();
+  }
+
+  editUser(user:any) {
+    this.isEditing = true;
+    this.user.name = user.name;
+    this.user.email = user.email;
+    this.user.contact = user.contact;
+  }
+
+  updateUser() {
+    const index = this.users.indexOf(this.user);
+    if (index !== -1) {
+      this.users[index] = { ...this.user };
+    }
+    this.isEditing = false;
+    this.resetData();
+  }
+
+  deleteUser(user:any) {
+    const index = this.users.indexOf(user);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+    }
   }
 
 }
